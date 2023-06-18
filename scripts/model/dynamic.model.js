ba65f1603566951318f9e1923b6f82f9ac92145a
zhihaofans <zhihaofans@hotmail.com>
2023-06-18 15:12:43 +0800

    动态支持番剧、视频笔记，修复番剧动态bug

--- scripts/model/dynamic.model.js
+++ scripts/model/dynamic.model.js
@@ -5,8 +5,31 @@ class DynamicItemData {
     $console.info(desc);
     // Dynamic info
     this.dynamic_id = desc.dynamic_id;
-    this.dynamic_type = desc.type; //8:视频,4308:直播,2:图文动态,1:转发,4:文字动态,0:文章
+    this.dynamic_type = desc.type; //8:视频,4308:直播,2:图文动态,1:转发,4:文字动态,0:文章,512:番剧,64:视频笔记
     switch (this.dynamic_type) {
+      case 64:
+        // 视频笔记
+        this.text = this.card.title;
+        this.image_list = this.card.image_urls || [];
+        if (this.card.banner_url) {
+          this.image_list.push(this.card.banner_url);
+        }
+        if (this.image_list.length === 0) {
+          this.image_list.push(this.card.author.face);
+        }
+        this.image = this.image_list[0];
+        this.article_id = this.card.id;
+        this.url = `bilibili://article/${this.article_id}`;
+        break;
+      case 512:
+        //番剧
+        this.text = this.card.apiSeasonInfo.title;
+        this.uname = this.card.new_desc || this.card.index || "未知番剧";
+        this.image = this.card.cover;
+        this.image_list = [this.image, this.card.apiSeasonInfo.cover];
+        this.url = this.card.url;
+        this.is_finish = this.card.apiSeasonInfo.is_finish;
+        break;
       case 0:
         this.text = this.card.title;
         this.image = this.card.banner_url;
@@ -37,8 +60,11 @@ class DynamicItemData {
         }
         break;
       case 4308:
+        // 直播
         this.text = this.card.live_play_info.title;
         this.image = this.card.live_play_info.cover;
+        this.is_live = this.card.live_play_info.live_status === 1;
+        this.url = this.card.live_play_info.link;
         break;
       default:
         this.text = this.card?.title || "开发中";
@@ -46,8 +72,11 @@ class DynamicItemData {
     }
 
     // User info
-    this.uid = desc.user_profile.info.uid;
-    this.uname = desc.user_profile.info.uname;
+    if (this.dynamic_type !== 512) {
+      // 番剧没有user_profile
+      this.uid = desc?.user_profile.info.uid;
+      this.uname = desc.user_profile.info.uname;
+    }
 
     //Video info
     this.bvid = desc.bvid;
